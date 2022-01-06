827a876bfa49bf80d40f408ee5d3aa6484b6b169
sdetphoenix <97071667+sdetphoenix@users.noreply.github.com>
2022-01-05 20:21:26 -0500

    Update Test.java
    Remote Commit

--- GitEclipseProject/src/Test.java
+++ GitEclipseProject/src/Test.java
@@ -4,4 +4,10 @@ public class Test {
 	//this is test
 	// the files must be in staged area for commit and push
 	// this is the second changes 
+	
+	
+	public void methodA(){
+		// This is from Remote Repository
+		
+	}
 }
